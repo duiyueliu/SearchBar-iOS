b223096bfe848f1b10f3dbec17ee7cdf4ba04628
Arthur Igberdin <art.igberdin@gmail.com>
2014-09-27 14:54:45 +0400

    Initial Commit

---
+++ SearchBar-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SearchBar-iOS
+//
+//  Created by Artur on 27/09/14.
+//  Copyright (c) 2014 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SearchBar-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SearchBar-iOS
+//
+//  Created by Artur on 27/09/14.
+//  Copyright (c) 2014 Artur Igberdin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
